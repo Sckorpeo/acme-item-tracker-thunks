2c7eaa1bff5d7376c1f93c213f4e98a02ee15ea0
Kieran B <kieranbru.web@gmail.com>
2022-07-13 19:23:22 -0400

    Added 3 thing limit for Users

--- db/index.js
+++ db/index.js
@@ -4,34 +4,53 @@ const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/the
 const { STRING, INTEGER } = Sequelize;
 
 const User = conn.define('user', {
-	name: {
-		type: STRING
-	},
-	ranking: {
-		type: INTEGER,
-		defaultValue: 5
-	}
+    name: {
+        type: STRING
+    },
+    ranking: {
+        type: INTEGER,
+        defaultValue: 5
+    }
 });
 
 const Thing = conn.define('thing', {
-	name: {
-		type: STRING
-	},
-	ranking: {
-		type: INTEGER,
-		defaultValue: 1
-	}
+    name: {
+        type: STRING
+    },
+    ranking: {
+        type: INTEGER,
+        defaultValue: 1
+    }
 });
 
 Thing.belongsTo(User);
+
 Thing.addHook('beforeValidate', (thing) => {
-	if (!thing.userId) {
-		thing.userId = null;
-	}
+    if (!thing.userId) {
+        thing.userId = null;
+    }
 });
 
+// Don't allow a user to be assigned more than 3 Things.
+Thing.beforeUpdate(async (thing, options) => {
+    // Get userId that is trying to be added to Thing.
+    const { userId } = thing.dataValues;
+    // Find all Things associated with userId.
+    if (userId) {
+        const instances = await Thing.findAll({
+            where: {
+                userId
+            }
+        });
+        // If there are 3 or more things assigned this userId. Throw an error.
+        if (instances.length >= 3) {
+            throw new Error("A user can't be assigned more than 3 things!")
+        }
+    }
+})
+
 module.exports = {
-	conn,
-	User,
-	Thing
+    conn,
+    User,
+    Thing
 };
--- src/Things.js
+++ src/Things.js
@@ -3,66 +3,79 @@ import ThingForm from './ThingForm';
 import { connect } from 'react-redux';
 import { deleteThing, updateThing } from './api-calls-thunks';
 
-const Things = ({ things, users, deleteThing, increment, updateThing }) => {
-	return (
-		<div>
-			<h1>Things</h1>
-			<ul>
-				{
-					things.map(thing => {
-						const user = users.find(user => user.id === thing.userId) || {};
 
-						return (
-							<li key={thing.id}>
-								{thing.name} ({thing.ranking})
-								owned by {user.name || 'nobody'}
-								<div>
-									<select defaultValue={thing.userId} onChange={ev => updateThing(thing, ev.target.value)}>
-										<option value=''>-- nobody --</option>
-										{
-											users.map(user => {
-												return (
-													<option key={user.id} value={user.id}>{user.name}</option>
-												);
-											})
-										}
-									</select>
-								</div>
-								<button onClick={() => deleteThing(thing)}>x</button>
-								<button onClick={() => increment(thing, -1)}>-</button>
-								<button onClick={() => increment(thing, 1)}>+</button>
+const Things = ({ things, users, deleteThing, increment, updateThing, hashMap = {} }) => {
+    // Create a hash map to track the number of things a user owns.
+    things.forEach(thing => {
+        if (!hashMap[thing.userId]) {
+            hashMap[thing.userId] = 1
+        }
+        else if (hashMap[thing.userId]) {
+            hashMap[thing.userId] += 1;
+        }
+    });
+    return (
+        <div>
+            <h1>Things</h1>
+            <ul>
+                {
+                    things.map(thing => {
+                        const user = users.find(user => user.id === thing.userId) || {};
 
-							</li>
-						);
-					})
-				}
-			</ul>
-			<ThingForm />
-		</div>
-	);
+                        return (
+                            <li key={thing.id}>
+                                {thing.name} ({thing.ranking})
+                                owned by {user.name || 'nobody'}
+                                <div>
+                                    <select defaultValue={thing.userId} onChange={ev => updateThing(thing, ev.target.value)}>
+                                        <option value='null'>-- nobody --</option>
+                                        {
+                                            users.map(user => {
+
+                                                if (hashMap[user.id] < 3 || !hashMap[user.id] || user.id === thing.userId) {
+                                                    return (
+                                                        <option key={user.id} value={user.id}>{user.name}</option>
+                                                    );
+                                                }
+                                            })
+                                        }
+                                    </select>
+                                </div>
+                                <button onClick={() => deleteThing(thing)}>x</button>
+                                <button onClick={() => increment(thing, -1)}>-</button>
+                                <button onClick={() => increment(thing, 1)}>+</button>
+
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+            <ThingForm />
+        </div>
+    );
 };
 
 export default connect(
-	(state) => {
-		return {
-			things: state.things,
-			users: state.users
-		}
-	},
-	(dispatch) => {
-		return {
-			updateThing: (thing, userId) => {
-				thing = { ...thing, userId: userId * 1 };
-				dispatch(updateThing(thing));
-			},
-			increment: (thing, dir) => {
-				thing = { ...thing, ranking: thing.ranking + dir };
-				dispatch(updateThing(thing));
-			},
-			deleteThing: (thing) => {
-				dispatch(deleteThing(thing));
-			}
-		};
+    (state) => {
+        return {
+            things: state.things,
+            users: state.users
+        }
+    },
+    (dispatch) => {
+        return {
+            updateThing: (thing, userId) => {
+                thing = { ...thing, userId: userId * 1 };
+                dispatch(updateThing(thing));
+            },
+            increment: (thing, dir) => {
+                thing = { ...thing, ranking: thing.ranking + dir };
+                dispatch(updateThing(thing));
+            },
+            deleteThing: (thing) => {
+                dispatch(deleteThing(thing));
+            }
+        };
 
-	}
+    }
 )(Things);
